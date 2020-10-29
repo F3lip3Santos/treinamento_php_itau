3eee857e5f5ca5be9b0d4df96436f903f56300d4
F3lip3Santos <tostafs@hotmail.com>
2020-10-29 14:36:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php $i = (9+1); ?>
-<h1>Bem vindo turma <?php echo "$i B"; ?> Mary Key</h1>
-<h2>Bem vindo turma <?php echo "$i B"; ?> Gestão de négocio</h>
+
+<h1>Bem vindo ao sistema Mary Key</h1>
+<h2>Bem vindo turma Gestão de négocio</h>
 
